dfe622fd790527a8ef50954aa8672ee2061dd900
NamSangwon <127469500+NamSangwon@users.noreply.github.com>
2023-05-08 23:13:06 +0900

    Delete workahallic directory

--- workahallic/test_app/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import models
-
-class YourModelSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models
-        fields = '__all__'
