37bb5e150bbe41bb25cf48365cfd5b3221d72334
Oh Soohyun <elena0315@naver.com>
2023-05-12 02:35:39 +0900

    feat register(insert into DB, urls mapping)

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('duplicate/', views.duplicateCheck, name="duplicatecheck"),
+    path('duplicate/', views.duplicateCheck, name="duplicateCheck"),
+    path('register/', views.register, name="register"),
 
+    # DB 확인을 위한 임시 - 나중에 지우기
     path('userdatas/', views.getUserDatas, name="userdatas"),
     path('postMember/', views.postMember, name="postMember"), # 이 경로로 json 데이터 던지면 db에 insert
 ]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,5 +1,4 @@
-# from django.shortcuts import render
-
+from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import User
@@ -8,6 +7,7 @@ from rest_framework import status
 
 # Create your views here.
 
+# 이메일 중복 확인
 def duplicateCheck(request):
     email = request.POST.get('email')
 
@@ -23,13 +23,21 @@ def duplicateCheck(request):
     context = {'duplicate' : duplicate}
     return Response(context)
 
+# 회원가입 시 DB에 data 저장
+def register(request):
+    userData = request.data
+    serializer = UserDataSerializer(data=userData)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
 
 
 
 
 
-
-
+# DB 확인을 위한 view 함수 - 나중에 지우기
 @api_view(['GET'])
 def getUserDatas(request):
     datas = User.objects.all()
