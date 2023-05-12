96da16386d5d75bdb5e899a64ea3647690705cef
NamSangwon <skatkdddnjs1@gmail.com>
2023-05-12 14:30:59 +0900

    fix: 로그인 및 회원가입 통합

---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('duplicate/', views.duplicateCheck, name="duplicateCheck"),
+    path('register/', views.register, name="register"),
+    path('login/', views.login, name="login"),
+
+    # DB 확인을 위한 임시 - 나중에 지우기
+    path('userdatas/', views.getUserDatas, name="userdatas"),
+    path('postMember/', views.postMember, name="postMember"), # 이 경로로 json 데이터 던지면 db에 insert
+]
---
+++ accounts/views.py
@@ -0,0 +1,77 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import User
+from .serializers import UserDataSerializer
+from rest_framework import status
+
+# 이메일 중복 확인
+@api_view(['POST'])
+def duplicateCheck(request):
+    reqData = request.data
+    email = reqData['email']
+
+    try:
+        _email = User.objects.get(email=email)
+    except:
+        _email = None
+        
+    if _email is None:
+        duplicate = "pass"
+    else:
+        duplicate = "fail"
+    context = {'duplicate' : duplicate}
+    return Response(context)
+
+# 회원가입 시 DB에 data 저장
+@api_view(['POST'])
+def register(request):
+    if request.method == 'POST':
+        reqData = request.data
+        serializer = UserDataSerializer(data=reqData)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# 로그인
+@api_view(['POST'])
+def login(request):
+    if request.method == 'POST':
+        reqData = request.data
+        
+        inputEmail = reqData['email']
+        inputPW = reqData['password']
+
+        # DB에 ID가 있는 여부에 따라 response
+        if User.objects.filter(email=inputEmail).exists():
+            getUser = User.objects.get(email=inputEmail)
+            # ID에 맞는 PW 인지 여부에 따라 response
+            if getUser.password == inputPW:
+                    return Response(status=status.HTTP_200_OK)
+            else:
+                return Response(status=status.HTTP_404_NOT_FOUND)
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+
+
+
+
+
+# DB 확인을 위한 view 함수 - 나중에 지우기
+@api_view(['GET'])
+def getUserDatas(request):
+    datas = User.objects.all()
+    serializer = UserDataSerializer(datas, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def postMember(request):
+    reqData = request.data
+    serializer = UserDataSerializer(data=reqData)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
