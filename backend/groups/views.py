9dcd1c6c9682d653a994110e17944ed74ea4e99d
NamSangwon <skatkdddnjs1@gmail.com>
2023-05-24 00:07:12 +0900

    feat: 그룹 멤버들의 시간표 통합 기능 추가

--- backend/groups/views.py
+++ backend/groups/views.py
@@ -3,7 +3,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
-from accounts.models import Group, GroupMember
+from accounts.views import restore_table, print_table
+from accounts.models import Group, GroupMember, User
 from accounts.serializers import GroupDataSerializer, GroupMemberSerializer
 from django.core.exceptions import ValidationError
 
@@ -66,3 +67,45 @@ def deleteGroup(self, code):
     
     group.delete()
     return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# 그룹 멤버들의 시간표 통합 
+@api_view(['GET'])
+def groupTable(request):
+    reqData = request.data
+    get_group_code = reqData['group_code']
+
+    if Group.objects.filter(group_code=get_group_code).exists():
+        get_group_members = GroupMember.objects.filter(group_code=get_group_code).values()
+
+        # 그룹 멤버 각각의 시간표 읽어 오기
+        members_time_table = []
+        for member in get_group_members:
+            group_user = User.objects.get(email=member['email_id']) # 해당 그룹 멤버
+            user_table = restore_table(group_user.email) # 해당 그룹 멤버의 시간표
+            members_time_table.append(user_table) 
+
+        # 그룹 멤버들의 시간표가 존재하면 그룹 시간표 통합
+        if members_time_table:
+            group_table = integrate_table(members_time_table)
+
+        return Response({"integrated table":group_table})
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+
+# 그룹 멤버들의 시간표를 통합하는 함수
+def integrate_table(members_time_table):
+    group_table = members_time_table[0] # 첫 시간표 미리 추가
+
+    # 시간표 통합
+    for idx in range(1, len(members_time_table)): # 그룹 멤버 각각의 시간표 (첫 시간표 건너뛰기)
+        for time in range(len(members_time_table[idx])): # 시간표의 각 시간대
+            for day in range(7): # 시간대의 각 요일
+                if group_table[time][day] == 0 or group_table[time][day] >= 2: # (그룹 테이블) 공강일 시
+                    if members_time_table[idx][time][day] == 1: # (개인 시간표) 강의 있을 시 => (그룹 테이블) 공강 -> 일정
+                        group_table[time][day] = 1
+                    else: # (개인 시간표) 강의 없을 시 => (그룹 테이블) 공강 -> 일정
+                        group_table[time][day] += members_time_table[idx][time][day]
+
+    return group_table
